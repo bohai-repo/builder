c6087305e8a1a8661124719da558d7fa6716eb0e
姚波海 <yaobohai@hd123.com>
2023-05-08 20:22:45 +0800

    add apps

--- build.sh
+++ build.sh
@@ -24,13 +24,13 @@ function github-runner() {
     cd ./github-runner
     export docker_version='20.10.7'
     wget https://github.com/actions/runner/releases/download/v${build_version}/actions-runner-linux-arm64-${build_version}.tar.gz
-    sleep 10
     tar xzf actions-runner-linux-arm64-${build_version}.tar.gz && rm -rf actions-runner-linux-arm64-${build_version}.tar.gz
+    ls -la && pwd
     sed -i '3,9d' ./config.sh && sed -i '3,8d' ./run.sh
-    tar -zcf actions-runner-linux-arm64-2.304.0.tar.gz *
-    sed -i "s/docker_version/${docker_version}/g" Dockerfile
-    sed -i "s/version_key/$build_version/g" Dockerfile
-    docker build . -t ${build_report}/${alias_app}:${build_version}
+#    tar -zcf actions-runner-linux-arm64-2.304.0.tar.gz *
+#    sed -i "s/docker_version/${docker_version}/g" Dockerfile
+#    sed -i "s/version_key/$build_version/g" Dockerfile
+#    docker build . -t ${build_report}/${alias_app}:${build_version}
 }
 
 function main() {
