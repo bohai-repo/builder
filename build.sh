22f13528de2aca0929971fa1ea84bfa95d530df6
姚波海 <yaobohai@hd123.com>
2023-05-08 13:45:00 +0800

    add apps

--- build.sh
+++ build.sh
@@ -6,13 +6,14 @@ build_version=$3
 build_report="registry.cn-hangzhou.aliyuncs.com/bohai_repo"
 
 function envfile() {
-  echo "start build: ${build_report}/${build_app}:${build_version}"
+  echo "start build: ${build_report}/${alias_app}:${build_version}"
+  echo "-------------------"
 }
 
 function frpc() {
     cd ./frpc
     sed -i "s/version_key/$build_version/g" Dockerfile
-    docker build . -t ${build_report}/${build_app}:${build_version}
+    docker build . -t ${build_report}/${alias_app}:${build_version}
 }
 
 
