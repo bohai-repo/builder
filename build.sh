e054fc9ad321ddd78fe1c81d9801bffa7750fde9
姚波海 <yaobohai@hd123.com>
2023-05-08 21:26:42 +0800

    add apps

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ function github-runner() {
     apt-get install wget -y
     mkdir docker && cd docker
     curl https://download.docker.com/linux/static/stable/aarch64/docker-${docker_version}.tgz --output docker-${docker_version}.tgz
-    tar zcf docker-${docker_version}.tgz && cd docker && tar zcf docker.tar.gz *
+    tar xzf docker-${docker_version}.tgz && cd docker && tar zcf docker.tar.gz *
     cd ../ && mv docker/docker.tar.gz ./
     mkdir build && cd build
     wget https://github.com/actions/runner/releases/download/v${build_version}/actions-runner-linux-arm64-${build_version}.tar.gz &>/dev/null
