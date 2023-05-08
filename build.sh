0ba6061b994e2bd4b73adcece10a26b6616cc88f
姚波海 <yaobohai@hd123.com>
2023-05-08 20:08:04 +0800

    add apps

--- build.sh
+++ build.sh
@@ -19,6 +19,19 @@ function frpc() {
     docker build . -t ${build_report}/${alias_app}:${build_version}
 }
 
+function github-runner() {
+    cd ./github-runner
+    export docker_version='20.10.7'
+    wget https://github.com/actions/runner/releases/download/v${build_version}/actions-runner-linux-arm64-${build_version}.tar.gz
+    tar xzf ./actions-runner-linux-arm64-${build_version}.tar.gz && rm -rf actions-runner-linux-arm64-2.278.0.tar.gz
+    sed -i '3,9d' ./config.sh
+    sed -i '3,8d' ./run.sh
+    tar -zcvf actions-runner-linux-arm64-2.304.0.tar.gz *
+    sed -i "s/docker_version/${docker_version}/g" Dockerfile
+    sed -i "s/version_key/$build_version/g" Dockerfile
+    docker build . -t ${build_report}/${alias_app}:${build_version}
+}
+
 function main() {
     launch
 }
