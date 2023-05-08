087ac60726b5b932085780201464605dee30faeb
姚波海 <yaobohai@hd123.com>
2023-05-08 23:19:46 +0800

    add apps

--- build.sh
+++ build.sh
@@ -20,6 +20,8 @@ function frpc() {
 }
 
 function github-runner() {
+    # 推送到日本仓
+    local build_report="registry.ap-northeast-1.aliyuncs.com/bohai_repo"
     cd ./github-runner
     export docker_version='20.10.7'
     apt-get install wget -y
@@ -35,7 +37,6 @@ function github-runner() {
     cd ../ && mv build/actions-runner-linux-arm64-${build_version}.tar.gz ./ && rm -rf build
     sed -i "s/docker_version/${docker_version}/g" Dockerfile
     sed -i "s/version_key/$build_version/g" Dockerfile
-    local build_report="registry.ap-northeast-1.aliyuncs.com/bohai_repo"
     docker build . -t ${build_report}/${alias_app}:${build_version}
 }
 
