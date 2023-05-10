799495898571decaa6175d09a1c818cb23c98c7e
姚波海 <yaobohai@hd123.com>
2023-05-10 09:41:42 +0800

    调整

--- github-runner/entrypoint.sh
+++ github-runner/entrypoint.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
 
-personal(){
+base_pack(){
     if [ -n "${ADDITIONAL_PACKAGES}" ]; then
     TO_BE_INSTALLED=$(echo ${ADDITIONAL_PACKAGES} | tr "," " " )
     echo "Installing additional packages: ${TO_BE_INSTALLED}"
-    sudo apt-get update && sudo apt-get install -y ${TO_BE_INSTALLED} && sudo apt-get clean
+        sudo apt-get update && sudo apt-get install -y ${TO_BE_INSTALLED} && sudo apt-get clean
     fi
+}
 
+personal(){
     registration_url="https://github.com/${GITHUB_OWNER}"
     token_url="https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
 
     if [ -n "${GITHUB_TOKEN}" ]; then
         echo "Using given GITHUB_TOKEN"
-
         if [ -z "${GITHUB_REPOSITORY}" ]; then
             echo "When using GITHUB_TOKEN, the GITHUB_REPOSITORY must be set"
             return
         fi
-
         registration_url="https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY}"
         export RUNNER_TOKEN=$GITHUB_TOKEN
-
     else
         if [ -n "${GITHUB_REPOSITORY}" ]; then
             token_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
@@ -50,10 +49,10 @@ personal(){
 }
 
 organizational(){
-    export registration_url="https://github.com/${GITHUB_ORG_NAME}"
-    export token_url="https://api.github.com/orgs/${GITHUB_ORG_NAME}/actions/runners/registration-token"
-    export payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${token_url})
-    export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+    registration_url="https://github.com/${GITHUB_ORG_NAME}"
+    token_url="https://api.github.com/orgs/${GITHUB_ORG_NAME}/actions/runners/registration-token"
+    payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${token_url})
+    RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
     if [ -z "${RUNNER_NAME}" ]; then
         RUNNER_NAME=$(hostname)
@@ -66,14 +65,6 @@ organizational(){
     ./config.sh --unattended --url https://github.com/${GITHUB_ORG_NAME} --token ${RUNNER_TOKEN} --labels "${RUNNER_LABELS}"
 }
 
-if [ $REGIST_TYPE = 'personal' ];then
-    personal
-elif [ $REGIST_TYPE = 'organizational' ];then
-    organizational
-else
-    personal
-fi
-
 remove() {
     if [ -n "${GITHUB_RUNNER_TOKEN}" ]; then
         export REMOVE_TOKEN=$GITHUB_RUNNER_TOKEN
@@ -84,6 +75,14 @@ remove() {
 
     ./config.sh remove --unattended --token "${RUNNER_TOKEN}"
 }
+
+if [ $REGIST_TYPE = 'personal' ];then
+    personal
+elif [ $REGIST_TYPE = 'organizational' ];then
+    organizational
+else
+    personal
+fi
 
 trap 'remove; exit 130' INT
 trap 'remove; exit 143' TERM
