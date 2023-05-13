7ada1b9a6ccc2c4e52f376bf1983420335478e78
姚波海 <yaobohai@hd123.com>
2023-05-13 16:25:36 +0800

    runner

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ function launch() {
   ${build_app}
   docker build . -t ${build_repo}/${alias_app}:${build_version}
   if [[ $? == 0 ]];then
-    docker push ${build_repo}/${alias_app}:${build_version} \
-    && docker rmi ${build_repo}/${alias_app}:${build_version}
+    docker push ${build_repo}/${alias_app}:${build_version} && docker rmi ${build_repo}/${alias_app}:${build_version}
   fi
 }
 
