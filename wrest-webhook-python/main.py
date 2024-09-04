ae8d2ed31f296896dd4d3a9610d64b70f387ab61
姚博海 <86307090+yaobohai@users.noreply.github.com>
2024-09-05 01:02:24 +0800

    wrest-webhook-python

--- wrest-webhook-python/main.py
+++ wrest-webhook-python/main.py
@@ -25,7 +25,7 @@ def api():
     headers = {'Content-Type': 'application/json;charset=utf-8'}
     payload = {
         "receiver": receiver,
-        "msg": f"{task_title}的更新: {title}\t文章地址: {link}"
+        "msg": f"{task_title}的更新: {title}\n文章地址: {link}"
     }
 
     try:
