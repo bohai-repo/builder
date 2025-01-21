93cde6f5e393a9b071c45e578c03380b2a753a23
姚波海 <yaobohai@hd123.com>
2025-01-21 14:54:25 +0800

    wrest-webhook-python增加ai总结功能

--- wrest-webhook-python/main.py
+++ wrest-webhook-python/main.py
@@ -1,18 +1,42 @@
 import os
 import re
 import requests
+from openai import OpenAI, OpenAIError
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+ai_api_key = os.environ.get('ai_api_key') or 'xxxxx'
+ai_api_enable = os.environ.get('ai_api_enable') or 'false'
 wrest_url = os.environ.get('wrest_url') or 'http://127.0.0.1:7600'
+
+def summarize_text(text):
+    try:
+        api_key = os.getenv("ai_api_key")
+        if not api_key:
+            raise ValueError("未配置openai的api key,请在环境变量中配置ai_api_key")
+
+        client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {"role": "system", "content": "你是一个信息提取总结的助手，我需要你对我给你的信息进行分析和总结，并返回简短的、35字以内的总结"},
+                {"role": "user", "content": text},
+            ],
+            stream=False
+        )
+
+        return response.choices[0].message.content
+
+    except (OpenAIError, ValueError) as e:
+        return "Ai 分析繁忙中,请稍后再试"
 
 @app.route('/api', methods=['POST', 'GET'])
 def api():
     # 消息接收方
     receiver = request.args.get('receiver', '47719964397@chatroom')
-    
     # 处理从RssPush发送过来的Post请求
     if request.method == 'POST':
         title = request.form.get('title')
@@ -39,7 +63,12 @@ def api():
             task_title = '洛阳实时天气'
         elif task_title == 'Kubernetes' and 'v2ex' in link:
             task_title = 'v2ex-k8s'
-        msg = f"{task_title}：{title}\n\n{link}"
+
+        if ai_api_enable:
+            summary = summarize_text(desp)
+            msg = f"{task_title}：{title}\n\n{link} \n\n\nAi总结(deepseek): {summary}"
+        else:
+            msg = f"{task_title}：{title}\n\n{link}"
     # 处理直接请求的消息发送
     elif request.method == 'GET':
         msg = request.args.get('msg')
