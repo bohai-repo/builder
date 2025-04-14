40ba284e8ff7139a3db22e3b064f0fa171c429e4
姚波海 <yaobohai@hd123.com>
2025-04-14 15:15:58 +0800

    add app: rss-manager

--- rss-manager/app/cron.after.js
+++ rss-manager/app/cron.after.js
@@ -41,7 +41,7 @@ async function do_action()
 
         
 
-        console.log(  "OLD LAST" , old_content , last_content );
+        console.log(  "源地址:" , old_content , "目标地址:", last_content );
         if( old_content &&  old_content != last_content )
         {
             // 如果设置了关键字，而文章标题不匹配关键字，那么跳过
