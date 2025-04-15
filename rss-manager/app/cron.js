609e14dce235a35756e0b4de28d1ccc6d951ec70
姚博海 <9803680+yaobohai@user.noreply.gitee.com>
2025-04-16 00:41:56 +0800

    减少 rss-manager 消息输出

--- rss-manager/app/cron.js
+++ rss-manager/app/cron.js
@@ -61,7 +61,8 @@ async function do_action()
         // console.log("[INFO] 轮训中... 共计 " + unique_keys.length + " 条任务，目前处理第" + (index + 1) + "个。旧文章地址:", old_content, "新文章地址:", last_content);
         if( old_content &&  old_content != last_content )
         {
-            console.log( `[INFO ] ${tasks[index].title} 更新了` );
+//            console.log( "Feed唯一值有变动" );
+            console.log(  "[INFO] "文章内容有更新:", last_content );
             // 如果白名单关键词检测，文章标题不匹配关键字，那么跳过
             const last_title = last.title?.toLowerCase();
             
